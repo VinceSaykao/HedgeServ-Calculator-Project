d5f0a2880d3531b76208e19a468170635b386c57
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-06-18 19:31:17 -0500

    added forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalcButtonsComponent } from './components/calc-buttons/calc-buttons.component';
 
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { CalcButtonsComponent } from './components/calc-buttons/calc-buttons.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
